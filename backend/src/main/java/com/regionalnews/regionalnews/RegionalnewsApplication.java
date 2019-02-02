f2a8991609314a39d504e62f3150ef71617d0750
kalqa <bartlomiejkalqa@gmail.com>
2019-02-02 11:52:53 +0100

    backend init

---
+++ backend/src/main/java/com/regionalnews/regionalnews/RegionalnewsApplication.java
@@ -0,0 +1,14 @@
+package com.regionalnews.regionalnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionalnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegionalnewsApplication.class, args);
+	}
+
+}
+
---
+++ backend/src/test/java/com/regionalnews/regionalnews/RegionalnewsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.regionalnews.regionalnews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegionalnewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
